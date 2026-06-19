c096c5ddaa50a6c831faac4f5872d36979f133f5
vatsal0 <vatsalbaherwani@gmail.com>
2026-06-18 23:05:29 -0700

    daily

--- blog/daily/posts.js
+++ blog/daily/posts.js
@@ -1,6 +1,7 @@
 // Shared posts data and nav helpers for daily blog entries.
 // Add entries here when you post. Format: "YYYY-MM-DD": "Title"
 const posts = {
+  "2026-06-18": "Edge of Sanity",
   "2026-06-17": "Rejection",
   "2026-06-12": "Velocity",
   "2026-06-09": "Food & The Brain",
